17c45a41e490401387237663e3972ed7931ece23
ayushpanwar2014 <ayush.panwar2014@gmail.com>
2025-08-22 17:20:38 +0530

    creating register page

---
+++ frontend/src/components/ui/Login.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Container, Box, Typography, Button, TextField, Link } from "@mui/material";
+function Login() {
+    return (
+
+        <Box
+            sx={{
+                width: "100%",
+                maxWidth: "400px",
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                marginTop: { xs: 10, lg: 3 },
+            }}
+        >
+            {/* Email */}
+            <TextField
+                fullWidth
+                variant="outlined"
+                label="Email"
+                type="email"
+                sx={{
+                    backgroundColor: "rgba(255,255,255,0.05)",
+                    borderRadius: 5,
+                    "& .MuiInputBase-input": {
+                        color: "whitesmoke",
+                    },
+                    "& .MuiInputLabel-root": {
+                        color: "whitesmoke",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                            border: 'none'
+                        },
+                        "&:hover fieldset": {
+                            border: 'none'
+                        },
+                        "&.Mui-focused fieldset": {
+                            border: 'none'
+                        },
+                    },
+                }}
+            />
+
+            {/* Password */}
+            <TextField
+                fullWidth
+                variant="outlined"
+                label="Password"
+                type="password"
+                sx={{
+                    backgroundColor: "rgba(255,255,255,0.05)", borderRadius: 5,
+                    "& .MuiInputBase-input": {
+                        color: "whitesmoke",
+                    },
+                    "& .MuiInputLabel-root": {
+                        color: "whitesmoke",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                            border: 'none'
+                        },
+                        "&:hover fieldset": {
+                            border: 'none'
+                        },
+                        "&.Mui-focused fieldset": {
+                            border: 'none'
+                        },
+                    },
+                }}
+            />
+
+            <Button
+                fullWidth
+                sx={{
+                    mt: 10, py: 1.5,
+                    background: "linear-gradient(135deg, #4e54c8 0%, #8f94fb 100%)", // gradient
+                    color: "white", fontWeight: "bold", fontSize: "1rem", borderRadius: "12px",
+                    boxShadow: "0 4px 15px rgba(0,0,0,0.3)",
+
+                }}
+            >
+                Login
+            </Button>
+        </Box>
+    )
+}
+
+export default Login
--- frontend/src/page/Homepage.jsx
+++ frontend/src/page/Homepage.jsx
@@ -1,6 +1,7 @@
 
 import { Container, Box, Typography, Button, TextField, Link } from "@mui/material";
 import MacOSButtons from "../components/ui/Controls";
+import Login from "../components/ui/Login";
 
 function HomePage() {
   return (
@@ -31,85 +32,7 @@ function HomePage() {
       </Typography>
 
       {/* Login Form */}
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: "400px",
-          display: "flex",
-          flexDirection: "column",
-          gap: 2,
-          marginTop: { xs: 10, lg: 3 },
-        }}
-      >
-        {/* Email */}
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Email"
-          type="email"
-          sx={{
-            backgroundColor: "rgba(255,255,255,0.05)",
-            borderRadius: 5,
-            "& .MuiInputBase-input": {
-              color: "whitesmoke",
-            },
-            "& .MuiInputLabel-root": {
-              color: "whitesmoke",
-            },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                border: 'none'
-              },
-              "&:hover fieldset": {
-                border: 'none'
-              },
-              "&.Mui-focused fieldset": {
-                border: 'none'
-              },
-            },
-          }}
-        />
-
-        {/* Password */}
-        <TextField
-          fullWidth
-          variant="outlined"
-          label="Password"
-          type="password"
-          sx={{ backgroundColor: "rgba(255,255,255,0.05)", borderRadius: 5,
-            "& .MuiInputBase-input": {
-              color: "whitesmoke",
-            },
-            "& .MuiInputLabel-root": {
-              color: "whitesmoke",
-            },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                border: 'none'
-              },
-              "&:hover fieldset": {
-                border: 'none'
-              },
-              "&.Mui-focused fieldset": {
-                border: 'none'
-              },
-            },
-          }}
-        />
-
-        <Button 
-          fullWidth
-          sx={{
-            mt: 10, py: 1.5,
-            background: "linear-gradient(135deg, #4e54c8 0%, #8f94fb 100%)", // gradient
-            color: "white", fontWeight: "bold", fontSize: "1rem", borderRadius: "12px",
-            boxShadow: "0 4px 15px rgba(0,0,0,0.3)",
-
-          }}
-        >
-          Login
-        </Button>
-      </Box>
+      <Login/>
 
           <span  className="createAccount">Create an Account</span>
 
